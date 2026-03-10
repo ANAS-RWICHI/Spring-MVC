266117113853645f487d499997bfe76dfc1b02f4
ANAS RWCHI <anassrwichi@gmail.com>
2026-03-10 22:05:22 +0000

    first commit

---
+++ src/main/java/com/example/spring_mvc/SpringMvcApplication.java
@@ -0,0 +1,44 @@
+package com.example.spring_mvc;
+
+import com.example.spring_mvc.entities.Product;
+import com.example.spring_mvc.repository.ProductRepo;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringMvcApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringMvcApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner start(ProductRepo productRepo){
+        return args ->{
+            productRepo.save(Product.builder()
+                    .name("Phone")
+                    .price(6000)
+                    .quantity(11)
+                    .build());
+            productRepo.save(Product.builder()
+                    .name("Laptop")
+                    .price(18000)
+                    .quantity(5)
+                    .build());
+            productRepo.save(Product.builder()
+                    .name("LG TV")
+                    .price(15000)
+                    .quantity(10)
+                    .build());
+            productRepo.save(Product.builder()
+                    .name("AirPods")
+                    .price(2500)
+                    .quantity(15)
+                    .build());
+        };
+    }
+
+}
