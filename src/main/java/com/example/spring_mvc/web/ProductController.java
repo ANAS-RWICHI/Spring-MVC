18020c005b2439d9d1f9f5ed7f6945a4012d35dd
Anas Rwichi <anassrwichi@gmail.com>
2026-03-17 00:57:37 +0000

    Add product management views and update controller for product handling

--- src/main/java/com/example/spring_mvc/web/ProductController.java
+++ src/main/java/com/example/spring_mvc/web/ProductController.java
@@ -2,10 +2,13 @@ package com.example.spring_mvc.web;
 
 import com.example.spring_mvc.entities.Product;
 import com.example.spring_mvc.repository.ProductRepo;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -15,7 +18,7 @@ public class ProductController {
     @Autowired
     private ProductRepo productRepo;
 
-    @GetMapping("/index")
+    @GetMapping({"/","index"})
     public String index(Model model) {
         List<Product> products = productRepo.findAll();
         model.addAttribute("productsList", products);
@@ -24,6 +27,19 @@ public class ProductController {
     @GetMapping("/delete")
     public String delete( @RequestParam(name="id") Long id) {
         productRepo.deleteById(id);
+        return "redirect:/index";
+    }
+    @GetMapping("/addProduct")
+    public String addProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "newProduct";
+    }
+    @PostMapping("/saveProduct")
+    public String saveProduct(@Valid Product product , BindingResult bindingResult , Model model) {
+        if (bindingResult.hasErrors()) {
+            return "newProduct";
+        }
+        productRepo.save(product);
         return "redirect:/index";
     }
 }
