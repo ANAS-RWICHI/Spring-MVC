f37f12c130b05be30151d67c940b979bcc66ef81
Anas Rwichi <anassrwichi@gmail.com>
2026-04-16 00:19:07 +0100

    Update product deletion method to use POST and implement form submission

--- src/main/java/com/example/spring_mvc/web/ProductController.java
+++ src/main/java/com/example/spring_mvc/web/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,7 +25,7 @@ public class ProductController {
         model.addAttribute("productsList", products);
         return "products";
     };
-    @GetMapping("/delete")
+    @PostMapping("/delete")
     public String delete( @RequestParam(name="id") Long id) {
         productRepo.deleteById(id);
         return "redirect:/index";
